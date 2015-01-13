09472c7a9e4f03f53b5a45bb8b8bcb823ca43716
Chris Mendis <chrism@format.com>
2015-01-13 10:27:11 -0500

    Create a Format-specific build process using Plumber
    Plumber is a (now dead) build tool that The Guardian decided to adopt
    for the Scribe lib. It may be interesting to port the lib over to a
    high-adoption build tool, such as Gulp.

--- Plumbing.js
+++ Plumbing.js
@@ -29,6 +29,11 @@ module.exports = function (pipelines) {
 
   /* jshint -W069 */
 
+  // We probably won't ever need this default build process at Format,
+  // since we're unlikely to start using RequireJS or another AMD dep
+  // management lib. Let's keep it around in case upstream decides to
+  // optimize/change the default build process. Merging upstream will
+  // be easier to do if this code block is kept intact.
   pipelines['build'] = [
     // TODO: use bower operation to find main of this component?
     // As per: https://github.com/bower/bower/issues/1090
@@ -37,5 +42,37 @@ module.exports = function (pipelines) {
     mainRequireJS,
     // Send the resource along these branches
     writeBoth
+  ];
+
+  // This is the build process for Format. It creates a UMD version of
+  // the Scribe library in the build/format directory. The file is called
+  // scribe.js. This is the file that we use in the format repo:
+  // http://bit.ly/1u0iqmP.
+  var rename = require('plumber-rename');
+  var umdRequireJS = requireJS({
+    preserveLicenseComments: false,
+    paths: {
+      'lodash-amd': '../../bower_components/lodash-amd',
+      'immutable': '../../bower_components/immutable'
+    },
+    include: ['../../src/scribe'],
+    wrap: {
+      start: "(function (root, factory) {\n" +
+      "  if (typeof define === 'function' && define.amd) {\n" +
+      "    define([], factory);\n" +
+      "  } else {\n" +
+      "    root.Scribe = factory();\n" +
+      "  }\n" +
+      "}(this, function () {\n\n",
+      end: "return require('scribe');\n" +
+      "}));"
+    }
+  });
+  var toFormatBuildDir = write('./build/format');
+  pipelines['format'] = [
+    glob('./node_modules/almond/almond.js'),
+    rename('scribe'),
+    umdRequireJS,
+    toFormatBuildDir.omitSourceMap
   ];
 };
